ff5db289bfedfbd4d348c15dffa212b6e1833308
YeonHyeEun <21900442@handong.edu>
2021-04-25 09:17:06 +0900

    7번

--- manager.c
+++ manager.c
@@ -41,7 +41,9 @@ void saveData(Product p[], int count){
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
 	
-	
+	for(int i=0; i < count ; i++) {
+		fprintf(fp, "%s \n", p[i].name);
+	}
 	fclose(fp);
 	printf("저장됨!\n");
 }
